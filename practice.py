c3b1b567e0864a8b64544824dd9c24f37196e03f
reb-mikealson <pratharajgupta@gmail.com>
2026-07-27 21:28:31 +0530

    URL analysis changes

--- practice.py
+++ practice.py
@@ -7,3 +7,16 @@ def analyzeURL(string):
         print("The URL is secure.")
     else:
         print("The URL is not secure.")
+    for i in string:
+        if i == ".":
+            dotCount += 1
+        elif i == "-":
+            hyphenCount += 1
+        elif i.isdigit():
+            digitCount += 1
+    print("Length of the URL:", urlLen)
+    print("Number of dots in the URL:", dotCount)
+    print("Number of hyphens in the URL:", hyphenCount)
+    print("Number of digits in the URL:", digitCount)
+string = input("Enter a URL: ")
+analyzeURL(string)
