30bfe5469988fe348557031167958e01cb61330e
reb-mikealson <pratharajgupta@gmail.com>
2026-07-29 13:55:29 +0530

    problem solve

---
+++ problem.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+cnt=0
+for i in range(num1,num2+1):
+    sum=0
+    if (i%3==0):
+        while(i>0):
+            sum+=i%10
+            i//=10
+        if(sum%2==0):
+            cnt+=1
+    else:
+        continue
+print(cnt)
